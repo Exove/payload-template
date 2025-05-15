ab3b9d9879350c7441110d2802fd8778dfea5f0c
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2025-05-15 11:14:54 +0300

    show articles

--- src/collections/articles.ts
+++ src/collections/articles.ts
@@ -13,7 +13,6 @@ const revalidateArticleHook: CollectionAfterChangeHook = async ({ doc, operation
 export const Articles: CollectionConfig = {
   slug: "articles",
   admin: {
-    hidden: true,
     useAsTitle: "title",
     group: "Pages",
     defaultColumns: ["title", "createdBy", "updatedAt", "createdAt"],
