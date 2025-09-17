a78f769cfa23012cc9b29d6f013f89e673bc5907
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2025-09-17 22:09:29 +0300

    fix locale type

--- src/app/(frontend)/[locale]/layout.tsx
+++ src/app/(frontend)/[locale]/layout.tsx
@@ -1,7 +1,6 @@
 import { Footer } from "@/components/Footer";
 import { Toaster } from "@/components/Toaster";
 import { SITE_NAME } from "@/lib/constants";
-import { Locale } from "@/types/locales";
 import type { Metadata } from "next";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages, setRequestLocale } from "next-intl/server";
@@ -13,14 +12,14 @@ const inter = Inter({ subsets: ["latin"] });
 type Props = {
   children: React.ReactNode;
   params: Promise<{
-    locale: Locale;
+    locale: string;
   }>;
 };
 
 export const metadata: Metadata = {
   title: SITE_NAME,
   icons: {
-    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg'/>",
+    icon: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 64 64'%3E%3Ccircle cx='32' cy='32' r='20' fill='blue'/%3E%3C/svg%3E",
   },
 };
 
