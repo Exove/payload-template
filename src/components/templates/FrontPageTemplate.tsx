185956bcf90b3e3ac365b0142a5a9efa623fe2aa
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2025-09-25 09:36:10 +0300

    style hero

--- src/components/templates/FrontPageTemplate.tsx
+++ src/components/templates/FrontPageTemplate.tsx
@@ -1,5 +1,6 @@
 import { BlockRenderer } from "@/components/BlockRenderer";
 import Button from "@/components/Button";
+import Heading from "@/components/Heading";
 import { Link } from "@/i18n/routing";
 import { parseLink } from "@/lib/parse-link";
 import { FrontPage } from "@/payload-types";
@@ -16,30 +17,34 @@ export default function FrontPageTemplate({ content }: FrontPageTemplateProps) {
   return (
     <main id="main-content">
       {hero && (
-        <section className="py-8">
-          <div className="mx-auto max-w-screen-lg px-4">
-            <h1 className="mb-4 text-3xl font-bold">{hero.title}</h1>
-            <p className="mb-6 text-lg">{hero.description}</p>
-            {typeof hero.image === "object" && hero.image?.url && (
-              <div className="mb-6">
-                <Image
-                  src={hero.image.url}
-                  alt={hero.image.alt || hero.title}
-                  width={800}
-                  height={400}
-                  className="rounded-lg"
-                />
-              </div>
-            )}
-            {linkUrl && (
-              <Button asChild={true}>
-                <Link href={linkUrl}>{linkLabel}</Link>
-              </Button>
-            )}
+        <section className="mb-24 mt-12 grid grid-cols-2 gap-8">
+          <div className="flex flex-col items-center justify-center">
+            <div>
+              <Heading level="h1" size="xl">
+                {hero.title}
+              </Heading>
+              <p className="mb-6 text-lg">{hero.description}</p>
+              {linkUrl && (
+                <Button asChild={true}>
+                  <Link href={linkUrl}>{linkLabel}</Link>
+                </Button>
+              )}
+            </div>
           </div>
+          {typeof hero.image === "object" && hero.image?.url && (
+            <div>
+              <Image
+                src={hero.image.url}
+                alt={hero.image.alt || hero.title}
+                width={800}
+                height={400}
+                className="aspect-video rounded-lg"
+              />
+            </div>
+          )}
         </section>
       )}
-      <div className="mx-auto max-w-screen-lg py-16">
+      <div className="mx-auto max-w-screen-lg">
         {content.content && <BlockRenderer blocks={content.content} />}
       </div>
     </main>
