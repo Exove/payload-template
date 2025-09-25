7c33ab579a74f2b569614eced01ff512a27eb122
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2025-09-25 16:19:37 +0300

    style accordions

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -12,17 +12,21 @@ export function AccordionTrigger({ children }: { children: React.ReactNode }) {
   return (
     <summary
       role="button"
-      className="mt-2 flex w-full cursor-pointer items-center justify-between gap-3 border-b border-purple-600 px-8 py-6 text-base font-bold leading-tight hover:underline group-open:bg-stone-800"
+      className="flex w-full cursor-pointer items-center justify-between gap-3 border-b border-stone-600 py-5 text-base font-medium leading-tight hover:underline group-open:border-transparent"
     >
       <div>{children}</div>
       <div className="relative h-6 w-6 shrink-0">
-        <PlusIcon className="absolute h-6 w-6 stroke-2 text-purple-600 transition-opacity duration-200 group-open:opacity-0" />
-        <MinusIcon className="absolute h-6 w-6 stroke-2 text-purple-600 opacity-0 transition-opacity duration-200 group-open:opacity-100" />
+        <PlusIcon className="absolute h-6 w-6 stroke-2 text-amber-600 transition-opacity duration-200 group-open:opacity-0" />
+        <MinusIcon className="absolute h-6 w-6 stroke-2 text-amber-600 opacity-0 transition-opacity duration-200 group-open:opacity-100" />
       </div>
     </summary>
   );
 }
 
 export function AccordionContent({ children }: { children: React.ReactNode }) {
-  return <div className="overflow-hidden px-8 py-8">{children}</div>;
+  return (
+    <div className="overflow-hidden border-b border-transparent px-0 py-4 group-open:border-stone-600">
+      {children}
+    </div>
+  );
 }
--- src/components/blocks/AccordionBlock.tsx
+++ src/components/blocks/AccordionBlock.tsx
@@ -1,16 +1,18 @@
+import { cn } from "@/lib/utils";
 import { AccordionBlock as AccordionBlockType } from "@/payload-types";
 import { AccordionContent, AccordionItem, AccordionTrigger } from "../Accordion";
 import { BlockRenderer, NodeTypes } from "../BlockRenderer";
 
 type AccordionProps = {
   block: AccordionBlockType;
+  className?: string;
 };
 
-export function AccordionBlock({ block }: AccordionProps) {
+export function AccordionBlock({ block, className }: AccordionProps) {
   const { items } = block;
 
   return (
-    <ul className="space-y-4">
+    <ul className={cn("my-6", className)}>
       {items?.map((item, index) => (
         <AccordionItem key={index}>
           <AccordionTrigger>{item.title}</AccordionTrigger>
